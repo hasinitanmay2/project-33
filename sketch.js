cb58a379fa8ffb952b8723721cde9d8bed8e7272
hasinitanmay2 <78693631+hasinitanmay2@users.noreply.github.com>
2021-07-24 15:59:21 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var bg,r;
+var snow = []
+var snow2 = []
+
+
+
+function preload() {
+ bg = loadImage("snow3.jpg");
+
+
+}
+
+
+
+function setup() {
+  createCanvas(800,400);
+ 
+  engine = Engine.create();
+ world = engine.world;
+
+}
+
+function draw() {
+  if(bg);
+  background(bg);
+  Engine.update(engine);
+  
+  fall();
+  falling();
+  
+  for(r = 0; r<snow.length; r++){
+    snow[r].display();
+  }
+  
+  for(s = 0; s<snow.length; s++){
+    snow2[s].display();
+  }
+  drawSprites();
+  
+}
+
+function fall(){
+  if(frameCount%5===0){
+    snow.push(new Snowflake(random(0,800),0,50,50));
+  }
+}
+
+function falling(){
+  if(frameCount%5===0){
+    newSnow.push(new Snowflake2(random(0,800),0,40,40));
+  }
+}
